427b2c6d99eb6850362c4a8604c7fb0b25d92ff3
Takashi Nishimura <tnisimu@roland.co.jp>
2017-03-03 11:41:07 +0900

    integrated filter.h preparing for maintaining as common header

--- src/create_filtered_wav/filters.h
+++ src/create_filtered_wav/filters.h
@@ -38,6 +38,43 @@ class CATT : public CFilterBase {
 	}
 };
 
+class CDELAY : public CFilterBase {
+  protected:
+	double	FS;
+	int     mDelay;
+	int     mIndex;
+	double* mDelayBuf;
+
+  public:
+	CDELAY(double theFS) {
+		FS = theFS;
+		mDelay = 0;
+		mIndex = 0;
+		mDelayBuf = 0;
+	}
+	~CDELAY() {
+		if (mDelayBuf) { delete [] mDelayBuf; }
+	}
+	void ShowCoef() {}
+	void SetProperty(double theDelay) {
+		mDelay = (int)(theDelay * FS + 0.5);
+		if (mDelayBuf) { delete [] mDelayBuf; }
+		mDelayBuf = new double[mDelay];
+		for (int i = 0; i < mDelay; i++) { mDelayBuf[i] = 0; }
+		mIndex = 0;
+	}
+	void ShowProperty() {
+		printf("DELAY\n");
+		printf(" delay %g (Sec)\n", mDelay / FS);
+	}
+	double Push(double theVal) {
+		int aCurIndex = mIndex;
+		mDelayBuf[aCurIndex] = theVal;
+		mIndex = (mIndex >= mDelay - 1) ? 0 : mIndex + 1;
+		return mDelayBuf[mIndex];
+	}
+};
+
 class CFA : public CFilterBase {
   protected:
 	double	FS;
@@ -360,6 +397,41 @@ class CPKG2 : public CEA {
 	}
 };
 
-			
+class CSINE : public CFilterBase {
+  protected:
+	double	FS;
+	double  mFreq;
+	double  mInitialPhase;
+	double 	phase_inc;
+	double	mCur_phase;
+
+  public:
+	CSINE(double theFS) {
+		FS = theFS;
+		phase_inc = 0;
+		mCur_phase = 0;
+	}
+	void ShowCoef() {}
+	void SetProperty(double theFreq, double theInitialPhaseAsDegree) {
+		mCur_phase = theInitialPhaseAsDegree * 2 * M_PI / 360.0;
+		phase_inc = 2 * M_PI * theFreq / FS;
+		mFreq = theFreq;
+		mInitialPhase = theInitialPhaseAsDegree;
+	}
+	void ShowProperty() {
+		printf("SINE\n");
+		printf(" freq %g (Hz)\n", mFreq);
+		printf(" initial_phase %g (degree)\n", mInitialPhase);
+	}
+	double Push(double theVal) {
+		double aVal = theVal * sin(mCur_phase);
+		mCur_phase += phase_inc;
+		if (mCur_phase > 2 * M_PI) {
+			mCur_phase -= 2 * M_PI;
+		}
+		return aVal;
+	}
+};
+
 
 #endif	/* _filters_h_ */
