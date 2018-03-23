37f64670853396866c4edd1e7368e4c1a4e4b1a3
Takashi Nishimura <tnisimu@roland.co.jp>
2018-03-23 10:09:55 +0900

    [mkWav] optimize

--- csv2wav.pl
+++ csv2wav.pl
@@ -68,33 +68,22 @@ sub usage {
 	{
 		my $unity_val = wav_util::GetUnityVal(\%infoHash);
 		while (my $dat = <IN>) {
-			if      (($infoHash{ "NUM_OF_CH" } == 3) && ($infoHash{ "FORMAT_ID" } == 3)) {	#3ch float
-				if ($dat =~ /(-?\d*(\d|\.)[-\de]*)\s*?[,\s]\s*?(-?\d*(\d|\.)[-\de]*)\s*?[,\s]\s*?(-?\d*(\d|\.)[-\de]*)/) {
-					my ($ch0, $ch1, $ch2) = (eval($1), eval($3), eval($5));
-					wav_util::WriteWavData($fhOut, \%infoHash, [$ch0, $ch1, $ch2]);
-#					print "3ch float val $ch0, $ch1, $ch2\n";
+			$dat =~ s/,\s+/,/g;
+			$dat =~ s/\s+/,/g;
+			my @dat_array = split(/,/, $dat);
+			foreach my $tmpDat (@dat_array) {
+				if ($infoHash{ "FORMAT_ID" } == 1) {
+#					$tmpDat = eval($tmpDat) / $unity_val;
+					$tmpDat = eval($tmpDat);
+				} else {
+					$tmpDat = eval($tmpDat);
 				}
-			} elsif (($infoHash{ "NUM_OF_CH" } == 2) && ($infoHash{ "FORMAT_ID" } == 3)) {	#stereo float
-				$dat =~ /(-?\d*(\d|\.)[-\de]*)\s*?[,\s]\s*?(-?\d*(\d|\.)[-\de]*)/;
-				wav_util::WriteWavData($fhOut, \%infoHash, [eval($1), eval($3)]);
-#				print "stereo float val $1/$2\n";
-			} elsif (($infoHash{ "NUM_OF_CH" } == 1) && ($infoHash{ "FORMAT_ID" } == 3)) {	#mono float
-				$dat =~ /(-?\d*(\d|\.)[-\de]*)/;
-				wav_util::WriteWavData($fhOut, \%infoHash, [eval($1)]);
-#				print "mono float val $1\n";
-			} elsif ($infoHash{ "NUM_OF_CH" } == 2) {	#stereo
-				$dat =~ /(-?0x[0-9a-f]+|-?\d+)\s*?[,\s]\s*(-?0x[0-9a-f]+|-?\d+)/i;
-				wav_util::WriteWavData($fhOut, \%infoHash, [eval($1) / $unity_val, eval($2) / $unity_val]);
-#				print "stereo decimal val $1/$2\n";
-			} elsif ($infoHash{ "NUM_OF_CH" } == 1) {	#mono
-				$dat =~ /(-?0x[0-9a-f]+|^-?\d+)/i;
-				wav_util::WriteWavData($fhOut, \%infoHash, [eval($1) / $unity_val]);
-#				print "mono decimal val $1\n";
-			} else {
-				print "skipped $dat";
-				next;
 			}
-			$wav_size += $infoHash{ "BLOCK_SIZE" };
+			$#dat_array = $infoHash{ "NUM_OF_CH" } - 1;	# adjust array size
+			if ($#dat_array >= 0) {
+				wav_util::WriteWavData($fhOut, \%infoHash, \@dat_array);
+				$wav_size += $infoHash{ "BLOCK_SIZE" };
+			}
 		}
 	}
 
