c738ceb044437cae9b2e230fd02cb7666052865e
Takashi Nishimura <tnisimu@roland.co.jp>
2024-07-17 15:35:12 +0900

    [levelmeter_ITU1770] add new tool to calculate loudness meter

---
+++ src/levelmeter_ITU1770/levelmeter_ITU1770.cpp
@@ -0,0 +1,183 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../filters.h"
+#include "../wav.h"
+
+
+void Usage(const char* command)
+{
+	fprintf(stderr, "report level on ITU-1770\n");
+	fprintf(stderr, "http://web.ck.miyakoda.roland.co.jp/piano/software/dsp/Loudness/Loudness.html\n");
+	fprintf(stderr, "%s input_wav_file output filename.wav\n\n", command);
+}
+
+void PrepareK_Filter(const SFormatChunk& formatChunk);
+void Destruct(const SFormatChunk& formatChunk);
+//void SetupWavHead(FILE* fp, const SFormatChunk& formatChunk, int dataSize);
+int Process(FILE *fp, FILE *theWavFile, const SFormatChunk& formatChunk, int dataSize, double theMeasurementInterval_sec);
+static void CopyFile(FILE* fpSrc, FILE* fpDst, int32_t copySize);
+
+int main(int argc, char* argv[])
+{
+	if (argc != 3) {
+		Usage(argv[0]);
+		exit(1);
+	}
+
+//	int wavLen = atoi(*++argv);
+	char* inWav = *++argv;
+	char* outfile = *++argv;
+
+	FILE *fpWav, *fpOut;
+
+	if ((fpWav = fopen(inWav, "rb")) == NULL) {
+		printf("Cannot open %s\n", inWav);
+		exit(1);
+	}
+	if ((fpOut = fopen(outfile, "wb")) == NULL) {
+		printf("Cannot open %s\n", outfile);
+		exit(1);
+	}
+
+	SFormatChunk formatChunkBuf;
+	int32_t dataSize = ParseWaveHeader(fpWav, &formatChunkBuf);
+	int32_t dataStartPos = ftell(fpWav);
+	rewind(fpWav);
+
+	if (dataSize > 0) {
+		PrepareK_Filter(formatChunkBuf);
+
+		CopyFile(fpWav, fpOut, dataStartPos);
+//		SetupWavHead(fpOut, formatChunkBuf, dataSize);
+		dataSize = Process(fpOut, fpWav, formatChunkBuf, dataSize, 0.4/*sec*/);
+		CopyFile(fpWav, fpOut, -1);
+		MaintainWavHeader(fpOut, dataSize, dataStartPos);
+
+		Destruct(formatChunkBuf);
+	}
+
+	fclose(fpWav);
+	fclose(fpOut);
+}
+
+#include "../list.h"
+static CList<CFilterBase*>* spList;
+void PrepareK_Filter(const SFormatChunk& formatChunk)
+{
+	int FSAMP = formatChunk.fsamp;
+	int num_ch = formatChunk.channel;
+	spList = new CList<CFilterBase*> [num_ch];
+
+	for (int ch = 0; ch < num_ch; ch++) {
+		{
+			CFilterBase* aIns = new CHPF2(FSAMP);      spList[ch].push_back(aIns);
+			((CHPF2*)aIns)->SetProperty(38.135470876113, 0.5);
+		}
+		{
+			CFilterBase* aIns = new CHSV2(FSAMP);    spList[ch].push_back(aIns);
+			((CHSV2*)aIns)->SetProperty(1681.97445095553, 4.0, 0.707);
+		}
+	}
+
+	for (CList<CFilterBase*>::Iterator iter = spList[0].begin(); iter != spList[0].end(); iter++) {
+		if (iter != 0) {
+			(*iter)->ShowProperty();
+			(*iter)->ShowCoef();
+		}
+	}
+}
+
+void Destruct(const SFormatChunk& formatChunk)
+{
+	int num_ch = formatChunk.channel;
+	for (int ch = 0; ch < num_ch; ch++) {
+		for (CList<CFilterBase*>::Iterator iter = spList[ch].begin(); iter != spList[ch].end(); iter++) {
+			if (iter != 0) {
+				delete *iter;
+			}
+		}
+	}
+	delete [] spList;
+}
+
+int Process(FILE *fp, FILE *theWavFile, const SFormatChunk& formatChunk, int dataSize, double theMeasurementInterval_sec)
+{
+	int wavSamples = dataSize / formatChunk.blockSize;
+	int FSAMP = formatChunk.fsamp;
+	int num_ch = formatChunk.channel;
+	int MeasurementInterval_sample = (int)(theMeasurementInterval_sec * FSAMP + 0.5);
+
+	double** buf = new double* [num_ch];
+	for (int ch = 0; ch < num_ch; ch++) {
+		buf[ch] = new double [MeasurementInterval_sample];
+		memset(buf[ch], 0, sizeof(double) * MeasurementInterval_sample);
+	}
+	double* sum = new double [num_ch];
+	for (int ch = 0; ch < num_ch; ch++) {
+		sum[ch] = 0;
+	}
+
+	int i = 0;
+	int aRingPointer = 0;
+	for (i = 0; i < MeasurementInterval_sample; i++) {
+		int aNewRingPointer = (aRingPointer + 1) % MeasurementInterval_sample;
+		for (int ch = 0; ch < num_ch; ch++) {
+			double val = ReadWaveData(theWavFile, &formatChunk);
+
+			for (CList<CFilterBase*>::Iterator iter = spList[ch].begin(); iter != spList[ch].end(); iter++) {
+				if (iter != 0) {
+					val = (*iter)->Push(val);
+				}
+			}
+			double mean_square = val * val / MeasurementInterval_sample * 0.9235/*-0.691dB*/;
+			sum[ch] += mean_square - buf[ch][aNewRingPointer];
+			buf[ch][aRingPointer] = mean_square;
+
+			WriteWaveData(fp, &formatChunk, 0);
+		}
+		aRingPointer = aNewRingPointer;
+	}
+	for (; i < wavSamples; i++) {
+		int aNewRingPointer = (aRingPointer + 1) % MeasurementInterval_sample;
+		for (int ch = 0; ch < num_ch; ch++) {
+			double val = ReadWaveData(theWavFile, &formatChunk);
+
+			for (CList<CFilterBase*>::Iterator iter = spList[ch].begin(); iter != spList[ch].end(); iter++) {
+				if (iter != 0) {
+					val = (*iter)->Push(val);
+				}
+			}
+			double mean_square = val * val / MeasurementInterval_sample * 0.9235/*-0.691dB*/;
+			sum[ch] = fmax(sum[ch] + mean_square - buf[ch][aNewRingPointer], 0);
+			buf[ch][aRingPointer] = mean_square;
+
+			WriteWaveData(fp, &formatChunk, sqrt(sum[ch]));
+		}
+		aRingPointer = aNewRingPointer;
+	}
+
+//	return (wavSamples - MeasurementInterval_sample) * formatChunk.blockSize;
+	return wavSamples * formatChunk.blockSize;
+}
+
+enum { BUF_SIZE = 0x100000, };
+static unsigned char buf[BUF_SIZE];
+
+static void CopyFile(FILE* fpSrc, FILE* fpDst, int32_t theSize)
+{
+	size_t remainSize = theSize;
+	size_t copySize = BUF_SIZE;
+	if ((remainSize >= 0) && (remainSize <= copySize)) { copySize = remainSize; }
+	while (copySize > 0) {
+		size_t readSize = fread(buf, 1, copySize, fpSrc);
+		if (readSize <= 0) { break; }
+		size_t writeSize = fwrite(buf, 1, readSize, fpDst);
+		if (writeSize <= 0) { break; }
+		if (remainSize >= 0) {
+			remainSize -= readSize;
+			if (remainSize <= copySize) { copySize = remainSize; }
+		}
+	}
+}
+
